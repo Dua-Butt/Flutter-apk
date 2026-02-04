7d5535d1229985d27f0104b098d56171eb84ff9a
DuaButt <bdua9833@gmail.com>
2026-02-04 19:51:37 +0500

    Push project to gitHub

---
+++ android/app/src/main/kotlin/flutter/app08/MainActivity.kt
@@ -0,0 +1,5 @@
+package flutter.app08
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ test/widget_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:hello_world/main.dart';
+
+void main() {
+  testWidgets('App displays Hello Flutter text', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that our app shows the correct text.
+    expect(find.text('Hello, Flutter app users!'), findsOneWidget);
+    expect(find.text('Hello World'), findsOneWidget);
+  });
+}
